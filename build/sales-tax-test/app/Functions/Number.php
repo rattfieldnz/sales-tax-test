b1e6ab6b1d38508d9666bb803a1b238c7cf05a9b
Rob Attfield <emailme@robertattfield.com>
2017-11-14 21:39:31 +1300

    Completed unit testing for Number functions class.

--- build/sales-tax-test/app/Functions/Number.php
+++ build/sales-tax-test/app/Functions/Number.php
@@ -15,14 +15,14 @@ class Number
     /**
      * Round a number to the nearest x value.
      *
-     * @param $num
-     * @param float $nearest
+     * @param           $num
+     * @param float|int $nearest
      *
      * @return float
      */
-    public static function roundNumber($num, $nearest = 0.05)
+    public static function roundNumber($num, $nearest = 5)
     {
-        return floatval(round($num / $nearest, 1, 1) * $nearest);
+        return floatval(self::roundUpToX($num, $nearest));
     }
 
     /**
@@ -48,13 +48,31 @@ class Number
      */
     public static function roundUpTo5Cents($value) 
     {
-
         $money = new Decimal2(strval($value));
         return floatval(strval(Decimal2::roundUpTo5Cents($money)));
     }
 
-    public static function pad($number)
-    {
 
+    /**
+     * @param $number
+     * @param int      $nearest
+     * @see https://github.com/ikr/money-math-php/blob/master/src/MoneyMath/Decimal2.php#L336
+     *
+     * @return \MoneyMath\Decimal2
+     */
+    public static function roundUpToX($number, int $nearest = 0) {
+        $dc2Number = new Decimal2(strval($number));
+        $lastDigit = intval(substr(strval($dc2Number), -1));
+        $addition = 0;
+
+        if($nearest == 0){
+            return $number;
+        }
+
+        if (gmp_mod($lastDigit, $nearest) != 0) {
+            $addition = '0.0' . strval($nearest - ($lastDigit % 5));
+        }
+
+        return floatval(strval(Decimal2::plus($dc2Number, new Decimal2($addition))));
     }
 }
--- build/sales-tax-test/tests/Unit/NumberTest.php
+++ build/sales-tax-test/tests/Unit/NumberTest.php
@@ -1,41 +1,61 @@
 <?php
 
+use App\Functions\Number;
 use Tests\TestCase;
 
 class NumberTest extends TestCase
 {
     public function setUp()
     {
-        $this->number = new \App\Functions\Number();
+        parent::setUp();
     }
 
     public function testRoundNumber()
     {
-        //TODO Implement test for roundNumber method
-        $this->assertTrue(false);
+        $this->assertEquals(2.00, Number::roundNumber(1.96));
+        $this->assertEquals(0.10, Number::roundNumber(0.09));
+        $this->assertEquals(27.90, Number::roundNumber(27.86));
+        $this->assertEquals(100, Number::roundNumber(99.99));
+        $this->assertEquals(23.50, Number::roundNumber(23.4567890123));
+        $this->assertEquals(0.05, Number::roundNumber(0.04));
+        $this->assertEquals(0.05, Number::roundNumber(0.02));
+        $this->assertEquals(0.03, Number::roundNumber(0.02,3));
+        $this->assertEquals(98765.45, Number::roundNumber(98765.43210));
+        $this->assertEquals(98765.44, Number::roundNumber(98765.43210,4));
+        $this->assertEquals(98765.43, Number::roundNumber(98765.43210,3));
     }
 
     public function testCalculateTax()
     {
-        //TODO Implement test for calculateTax method
-        $this->assertTrue(false);
+        $this->assertEquals(0, Number::calculateTax(12.49,0));
+        $this->assertEquals(4.75, Number::calculateTax(47.50, 10));
+        $this->assertEquals(2.40, Number::calculateTax(47.50, 5));
+        $this->assertEquals(1.00, Number::calculateTax(100, 1));
+        $this->assertEquals(0, Number::calculateTax(0, 0));
+        $this->assertEquals(-1.00, Number::calculateTax(-10, 10));
     }
 
     public function testRoundUpTo5Cents()
     {
-        //TODO Implement test for roundUpTo5Cents method
-        $this->assertTrue(false);
+        $this->assertEquals(12.50, Number::roundUpTo5Cents(12.49));
+        $this->assertEquals(1.25, Number::roundUpTo5Cents(1.23456789));
+        $this->assertEquals(0.00, Number::roundUpTo5Cents(0.00));
+        $this->assertEquals(0.05, Number::roundUpTo5Cents(0.01));
+        $this->assertEquals(99.95, Number::roundUpTo5Cents(99.951));
+        $this->assertEquals(100, Number::roundUpTo5Cents(99.96));
     }
 
-    public function testPad()
-    {
-        //TODO Implement test for pad method
-        $this->assertTrue(false);
+    public function testRoundUpToX(){
+        $this->assertEquals(12.49, Number::roundUpToX(12.49, 0));
+        $this->assertEquals(12.491, Number::roundUpToX(12.491, 0.1));
+        $this->assertEquals(12.50, Number::roundUpToX(12.49, 5));
+        $this->assertEquals(12.49, Number::roundUpToX(12.49, 1));
+        $this->assertEquals(0, Number::roundUpToX(0));
     }
 
     public function tearDown()
     {
-        //TODO Implement tearDown method
+        parent::tearDown();
     }
 
 }
